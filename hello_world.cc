7577331419563aeb4707be387da2d4ad2f34a6c0
MagikarpUsedFly <wr140815@ohio.edu>
2019-02-03 13:13:06 -0500

    initial commit with hellow_world.cc for parallel program

---
+++ hello_world.cc
@@ -0,0 +1,24 @@
+// Wesley Ryder
+// OpenMP
+#include <omp.h>
+#include <iostream>
+
+using namespace std;
+
+
+void test()
+{
+  #pragma omp critical
+  {
+    cout << "Hello from thread " << omp_get_thread_num() << endl;
+    cout << "This is Wes Ryder's first parallel program" << endl;
+    cout << "There are currently "
+    <<  omp_get_num_threads() <<  " threads running" << endl;
+  }
+}
+
+int main()
+{
+#pragma omp parallel
+  test();
+}
